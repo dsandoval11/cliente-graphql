504ed5287b2468c74dc85c7e1f1a81b0ece1f2da
David Sandoval <david281199@gmail.com>
2020-01-22 07:36:05 -0500

    Configuracion ESLint

--- src/components/Clientes/NuevoCliente.tsx
+++ src/components/Clientes/NuevoCliente.tsx
@@ -1,102 +1,127 @@
-import React, { Component, ChangeEvent, HtmlHTMLAttributes } from 'react'
+import React, { Component, ChangeEvent } from 'react';
 
-class NuevoCliente extends Component {
+interface AppState {
+  nombre: string;
+  apellido: string;
+  empresa: string;
+  emails: [
+    { email: string },
+  ];
+  edad: string;
+  tipo: string;
+}
 
-  state = {
-    nombre: '',
-    apellido: '',
-    empresa: '',
-    emails: [
-      { email: '' }
-    ],
-    edad: '',
-    tipo: ''
+class NuevoCliente extends Component<any, AppState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      nombre: '',
+      apellido: '',
+      edad: '',
+      emails: [{ email: '' }],
+      empresa: '',
+      tipo: '',
+    };
   }
 
-  handleChange = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  handleChange = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>): void => {
     if (event.target.name === 'emails') {
-      let { emails } = this.state
+      const { emails } = this.state;
       emails[0].email = event.target.value;
-      this.setState((prevState: any) => ({
+      this.setState((prevState: AppState) => ({
         ...prevState,
-        emails
-      }))
+        emails,
+      }));
     } else {
-      this.setState({
-        ...this.state,
-        [event.target.name]: event.target.value
-      });
+      const { name, value } = event.target;
+      this.setState((prevState: AppState) => ({
+        ...prevState,
+        [name]: value,
+      }));
     }
   }
 
   render() {
     return (
-      <React.Fragment>
-        <h1 className="text-center">Nuevo cliente</h1>
-        <div className="row justify-content-center">
-          <form className="col-md-8 m-3" >
-            <div className="form-row">
-              <div className="form-group col-md-6">
+      <>
+        <h1 className='text-center'>Nuevo cliente</h1>
+        <div className='row justify-content-center'>
+          <form className='col-md-8 m-3'>
+            <div className='form-row'>
+              <div className='form-group col-md-6'>
                 <label>Nombre</label>
-                <input type="text"
-                  name="nombre"
-                  className="form-control"
-                  placeholder="Nombre"
-                  onChange={this.handleChange} />
+                <input
+                  type='text'
+                  name='nombre'
+                  className='form-control'
+                  placeholder='Nombre'
+                  onChange={this.handleChange}
+                />
               </div>
-              <div className="form-group col-md-6">
+              <div className='form-group col-md-6'>
                 <label>Apellido</label>
-                <input type="text"
-                  name="apellido"
-                  className="form-control"
-                  placeholder="Apellido"
-                  onChange={this.handleChange} />
+                <input
+                  type='text'
+                  name='apellido'
+                  className='form-control'
+                  placeholder='Apellido'
+                  onChange={this.handleChange}
+                />
               </div>
             </div>
-            <div className="form-row">
-              <div className="form-group col-md-6">
+            <div className='form-row'>
+              <div className='form-group col-md-6'>
                 <label>Empresa</label>
-                <input type="text"
-                  name="empresa"
-                  className="form-control"
-                  placeholder="Empresa"
-                  onChange={this.handleChange} />
+                <input
+                  type='text'
+                  name='empresa'
+                  className='form-control'
+                  placeholder='Empresa'
+                  onChange={this.handleChange}
+                />
               </div>
-              <div className="form-group col-md-6">
+              <div className='form-group col-md-6'>
                 <label>Email</label>
-                <input type="email"
-                  name="emails"
-                  className="form-control"
-                  placeholder="Email"
-                  onChange={this.handleChange} />
+                <input
+                  type='email'
+                  name='emails'
+                  className='form-control'
+                  placeholder='Email'
+                  onChange={this.handleChange}
+                />
               </div>
             </div>
-            <div className="form-row">
-              <div className="form-group col-md-6">
+            <div className='form-row'>
+              <div className='form-group col-md-6'>
                 <label>Edad</label>
-                <input type="text"
-                  name="edad"
-                  className="form-control"
-                  placeholder="Edad"
-                  onChange={this.handleChange} />
+                <input
+                  type='text'
+                  name='edad'
+                  className='form-control'
+                  placeholder='Edad'
+                  onChange={this.handleChange}
+                />
               </div>
-              <div className="form-group col-md-6">
+              <div className='form-group col-md-6'>
                 <label>Tipo Cliente</label>
-                <select className="form-control"
-                  name="tipo"
-                  onChange={this.handleChange}>
-                  <option value="">Elegir...</option>
-                  <option value="PREMIUM">PREMIUM</option>
-                  <option value="BASICO">BÁSICO</option>
+                <select
+                  className='form-control'
+                  name='tipo'
+                  onChange={this.handleChange}
+                >
+                  <option value=''>Elegir...</option>
+                  <option value='PREMIUM'>PREMIUM</option>
+                  <option value='BASICO'>BÁSICO</option>
                 </select>
               </div>
             </div>
-            <button type="submit" className="btn btn-success float-right">Guardar Cambios</button>
+            <button type='submit' className='btn btn-success float-right'>Guardar Cambios</button>
           </form>
         </div>
-      </React.Fragment>
-    )
+      </>
+    );
   }
 }
 
-export default NuevoCliente
+export default NuevoCliente;
--- src/queries/index.js
+++ src/queries/index.js
@@ -7,4 +7,4 @@ export const CLIENTES_QUERY = gql`{
     apellido
     empresa
   }
-}`
+}`;
